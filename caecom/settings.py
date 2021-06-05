ea665198e94305a599794b2a3931db839c557305
Edjair Aguiar <edjairaguiar_@hotmail.com>
2021-06-05 15:10:41 -0300

    .

--- caecom/settings.py
+++ caecom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^e^ezi74u3ed+y++za^pb1wexej$kwnc#gx)w8q^4h^gw)k%rm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
